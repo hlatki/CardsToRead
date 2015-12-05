44751a6e487baf66c12e25c55ef45dbeed9bf904
hlatki <hannahatkinson@gmail.com>
2015-12-04 21:40:51 -0800

    Hot garbage. Trying to authorize with Trello.

--- src/cards-to-read/popup.cljs
+++ src/cards-to-read/popup.cljs
@@ -4,7 +4,20 @@
             [cljs.core.async :refer [>! <!]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
+(defn trello-auth []
+      (.authorize js/Trello
+                  {name: "CardsToRead",
+                   scope: {
+                           read: true,
+                           write: true },
+                   expiration: "never",
+                   (console/log "Authorization was successful :)"),
+                   (console/log "Authorization failed :("),
+                   }))
 (defn init []
+      (trello-auth)
   (let [bg (runtime/connect)]
     (go (>! bg :lol-i-am-a-popup)
         (console/log "Background said: " (<! bg)))))
+
+
